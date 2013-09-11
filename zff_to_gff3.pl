36ff11aea1cf615f19eeef3ca91dfccf3b58f4cf
rdandekar <ravi.dandekar@gmail.com>
2013-09-11 09:43:35 -0700

    Added script to convert the format of Core gene annotation files (zff to gff3) -	script modified from zff2gff3.pl within 	HMMstar

---
+++ zff_to_gff3.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use fatal;
+
+
+die "convert_ann_to_gff.pl <GFF>" unless @ARGV == 1;
+my ($zff_file) = @ARGV;
+
+my %Feature = (
+	'Exon'  => 'CDS',
+	'Einit' => 'CDS',
+	'Eterm' => 'CDS',
+	'Esngl' => 'CDS',
+);
+my $seq;
+my %H;
+
+open(ZFF, "<$zff_file") or die "Error reading GFF\n";
+while (<ZFF>) {
+	if (/^>(\S+)/) {
+		$seq = $1;
+	} else {
+		my @f = split;
+		if (@f == 4) {
+			my $strand = $f[1] < $f[2] ? '+' : '-';
+			if ($strand eq '-') {($f[1], $f[2]) = ($f[2], $f[1])}
+			print join("\t", $seq, 'snap', $Feature{$f[0]}, $f[1], $f[2], '.',
+				$strand, '.', $f[3]), "\n";
+		} elsif (@f == 9) {
+			print join("\t", $seq, 'verified', $Feature{$f[0]}, $f[1], $f[2], '.',
+				$f[3], '.', $f[8]), "\n";
+		} else {die "input does not appear to be ZFF"}
+	}
+}
