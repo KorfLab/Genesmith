8496fb8586e8e17e323af89f7c0e7225ee090d4d
rdandekar <ravi.dandekar@gmail.com>
2016-03-04 12:35:07 -0800

    expanded donor and acceptor parameter ranges

--- run_hmmgen.pl
+++ run_hmmgen.pl
@@ -16,8 +16,9 @@ getopts('hb');
 
 # DEFAULT settings [options]
 my $MAX_ORDER  = 5;      # The Highest order of emissions to be generated
-my $L_DON      = 9;      # Quantity of Donor States [5-9]
-my $L_ACCEP    = 30;     # Quantity of Acceptor States [10-30]
+my $L_DON      = 9;      # Quantity of Donor States [2-9]
+#my $L_ACCEP    = 30;     # Quantity of Acceptor States [10-30]
+my @l_accep    = (2, 5, 10, 15, 20, 25, 30);
 my $INTERGENIC = 1000;   # Length of Upstream/Downstream regions parsed for training [100-1000]
 
 die "
@@ -34,9 +35,9 @@ my ($GFF, $FASTA) = @ARGV;
 
 if ($opt_b) {
 	for (my $i=0; $i <= $MAX_ORDER; $i++) {
-		for (my $d=5; $d < $L_DON+1;$d++) {
-			for (my $a=10; $a < $L_ACCEP+1; $a+=5) {
-				`hmmgen_branch.pl -i $i -b $i -D $d -A $a $GFF $FASTA`;
+		for (my $d=2; $d < $L_DON+1;$d++) {
+			for (my $a=0; $a < @l_accep; $a++) {
+				`hmmgen_branch.pl -i $i -b $i -D $d -A $l_accep[$a] $GFF $FASTA`;
 			}
 		}
 	}
@@ -46,15 +47,15 @@ if ($opt_b) {
 # Generate Gene model states from order 0 - 5
 for (my $i=0; $i <= $MAX_ORDER; $i++) {
 	for(my $e=100; $e < $INTERGENIC+1; $e+=100) {
-		`hmmgen_gene.pl -5 $i -m $i -c $i -d $i -i $i -a $i -s $i -3 $i -D 5 -A 10 -U $e -E $e $GFF $FASTA`;
+		`hmmgen_gene.pl -5 $i -m $i -c $i -d $i -i $i -a $i -s $i -3 $i -D 2 -A 2 -U $e -E $e $GFF $FASTA`;
 	}
 }
 
 for (my $i=0; $i <= $MAX_ORDER; $i++) {
-	for(my $d=5; $d < $L_DON+1; $d++) {
-		for(my $a=10; $a < $L_ACCEP+1; $a+=5) {
-			next if $d == 5 and $a == 10;
-			`hmmgen_gene.pl -5 $i -m $i -c $i -d $i -i $i -a $i -s $i -3 $i -D $d -A $a $GFF $FASTA`;
+	for(my $d=2; $d < $L_DON+1; $d++) {
+		for(my $a=0; $a < @l_accep; $a++) {
+			next if $d == 2 and $a == 2;
+			`hmmgen_gene.pl -5 $i -m $i -c $i -d $i -i $i -a $i -s $i -3 $i -D $d -A $l_accep[$a] $GFF $FASTA`;
 		}
 	}
 }
